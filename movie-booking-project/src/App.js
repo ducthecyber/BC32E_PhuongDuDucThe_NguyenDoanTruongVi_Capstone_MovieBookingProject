1bbd783b1bd6c373b268e37d8cf34a2914f11b77
ducthecyber <phuongduducthe@gmail.com>
2022-10-18 01:02:41 +0700

    file structure

---
+++ movie-booking-project/src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <div className="w-96 bg-white shadow rounded text-red-300 text-3xl">
+          w-96
+        </div>
+        <div className="w-80 bg-white shadow rounded">
+          w-80
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
