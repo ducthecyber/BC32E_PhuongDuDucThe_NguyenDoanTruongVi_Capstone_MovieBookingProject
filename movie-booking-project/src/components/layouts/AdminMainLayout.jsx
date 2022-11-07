e93aeffd9747a4ee1689195d8887283b84bba1bc
Ty-Doan <tydoan.28052001@gmail.com>
2022-11-07 19:33:08 +0700

    push code sua giao dien khach hang + file phan chia viec

--- movie-booking-project/src/components/layouts/AdminMainLayout.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AdminMainLayout = () => {
-  return (
-    <div>AdminMainLayout</div>
-  )
-}
-
-export default AdminMainLayout
--- movie-booking-project/src/store/reducers/demoReducer.js
+++
@@ -1,9 +0,0 @@
-const stateDefault = {
-
-}
-
-export const demoReducer = (state = stateDefault, { payload, type }) => {
-    switch (type) {
-        default: return state
-    }
-}
